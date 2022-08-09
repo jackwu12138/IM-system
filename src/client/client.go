d533941201b06f586f87c2db8342da97f156b867
jackwu <2280284307@qq.com>
2022-08-09 12:58:12 +0800

    客户端实现菜单功能

--- src/client/client.go
+++ src/client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -10,12 +11,14 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
 	client := &Client{
 		ServerIP:   serverIP,
 		ServerPort: serverPort,
+		flag:       -1,
 	}
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", client.ServerIP, client.ServerPort))
 	if err != nil {
@@ -27,10 +30,54 @@ func NewClient(serverIP string, serverPort int) *Client {
 	return client
 }
 
+func (c *Client) Menu() bool {
+	var n int
+	fmt.Println("1. chatroom")
+	fmt.Println("2. private chat")
+	fmt.Println("3. modify username")
+	fmt.Println("0. exit")
+
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		fmt.Println("error input")
+		return false
+	}
+	if n > 3 || n < 0 {
+		fmt.Println("error input")
+		return false
+	}
+	return true
+}
+
+func (c *Client) Run() {
+	for c.flag != 0 {
+		for !c.Menu() {
+		}
+		switch c.flag {
+		case 1:
+			break
+		case 2:
+			break
+		case 3:
+			break
+		}
+	}
+}
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "Set the ip address of the server")
+	flag.IntVar(&serverPort, "port", 8888, "Set the server port number")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
-		fmt.Println("Failed to start client")
+		fmt.Println("Failed to start client [" + fmt.Sprintf("%s:%d", serverIp, serverPort) + "]")
 		return
 	}
 	fmt.Println("Start the client successfully")
